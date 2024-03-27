904caff1f671c9065f843298863ff5eaa6426fe1
ShauryaKAJLA <shauryakajla10@gmail.com>
2024-03-27 21:47:47 +0530

    Data sender adder from json

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const express =require('express')
 const app= express()
 
 const port=4000
+const data={
+    name:"shaurya",
+    rollno:2210997216,
+    marks:10
+}
 app.get('/',(req,res)=>{
    res.send("Kya haal hain bhai ke")
 })
-
+app.get('/profile',(req,res)=>{
+   res.json(data)
+})
 
 app.listen(process.env.PORT,()=>{
     console.log("Listening at port "+port)
